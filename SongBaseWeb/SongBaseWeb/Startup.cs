9e421e47939f5a05037aaaefa202c852a65a844c
Bill Zitomer <bill@zitomerconsulting.com>
2015-12-18 14:27:13 -0500

    SongBase Web App created.

---
+++ SongBaseWeb/SongBaseWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SongBaseWeb.Startup))]
+namespace SongBaseWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
